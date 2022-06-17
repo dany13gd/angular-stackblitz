26e02ebf455920b472a19236ff570ef2ea311c0f
dany13gd <dany13gd@gmail.com>
2022-06-16 21:33:41 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-spku2h-jfhj8v
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-spku2h-jfhj8v)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css'],
+})
+export class ProductAlertsComponent {
+  @Input() product!: Product;
+  // notify viene del padre
+  @Output() notify = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
